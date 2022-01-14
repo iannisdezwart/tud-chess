99a3bcceba9a1add61822794c4d6fb50bce45001
Eugene Wu <96541783+epic-username@users.noreply.github.com>
2022-01-14 08:54:51 +0100

    Add files via upload

--- front-end/js/splash.js
+++ front-end/js/splash.js
@@ -1,19 +1,23 @@
+
 var ws = new WebSocket(`ws://${location.host}`);
 
 /**
  * wrapper function for websocket send, it sends an JSON object to the server
  */
 const sendData = (data) => {
-    if (ws.readyState == 1) {
+    if (ws.readyState === 1) {
         ws.send(JSON.stringify(data));
+        console.log('Client data send');
     }
     else {
         ws.addEventListener('open', () =>
         {
             ws.send(JSON.stringify(data));
+            console.log('Client data send');
         }
         );
     }
+    console.log('data sent was: '+JSON.stringify(data));
 }
 
 /**
@@ -23,56 +27,44 @@ const sendData = (data) => {
  */
 function Listener(type, callback) {
         this.listen = function() {
-        ws.addEventListener('message', data => {//listen for server data
-            try {
-                dataObj = JSON.parse(data); //try to parse string data as an object
-            }
-            catch {//if data is not an object 
-                return;//do nothing
-            }
+        ws.addEventListener('message', event => {//listen for server event, note that event.type can be open, close, message, etc. but in this case we are only listening for event.type = message
+            console.log('data type received was '+ typeof event);//it seems that the servers sends back a string, which is automatically parsed as object
 
-            if (dataObj.type === type) { //data objects sent from the server should have attribute type
-                callback(dataObj);//if the actual data type is the same as the expected data type, call callback
+            console.log(`data is ${JSON.stringify(event.data)}`);
+            data = JSON.parse(event.data)//the data attribute of event contains 
+            if (data.type == type) { //data objects sent from the server should have attribute type
+                console.log('listener for data of type '+type+' reiceved data')
+                callback(data);//if the actual data type is the same as the expected data type, call callback
+                
             }
             else return;//else do nothing
         }
         )
+        console.log('listener for data of type '+type+' has started');
     }
 }
 
 
 
-/**
- * creates a new Promise object that gets the user token
- */
-function userToken() {
-    return new Promise(function(resolve) {//resolve is a callback function
-        if (sessionStorage.getItem('user-token') === null) {//if there is no user-token
-            sendData({type: 'get-user-token'});
-            tokenListener = new Listener('user-token', data => {
-                sessionStorage.setItem('user-token', data.token);
-                resolve(data.token);
-            });
-            tokenListener.listen();
-        }
-        else {
-            resolve(sessionStorage.getItem('user-token'));
-        }
-    });
-}
 
 
-
-/**
- * function to join game, async to use await
- */
-async function joinGame() {
-    localStorage.setItem('username', username.value === null? 'Anonymous':username.value);//store username locally
-    sendData({type: 'join-game', token: await userToken(), username: localStorage.getItem('username')});//wait for user token from server;
+function joinGame() {
     document.querySelector('#start').innerText = 'Joining...';
-    document.querySelector('#start').innerText = true;
-}
+    document.querySelector('#start').disabled = true;
+    localStorage.setItem('username', username.value === ''? 'Anonymous':username.value);//store username locally
+    if (sessionStorage.getItem('user-token') !== null) {//if there is a user-token already
+        sendData({type: 'join-game', token: sessionStorage.getItem('user-token'), username: localStorage.getItem('username')});//wait for user token from server;
+    }
 
+    else {
+        sendData({type: 'get-user-token'});//else request user token
+        tokenListener = new Listener('user-token', data => {//once token is recieved, store it, then send join game request
+            sessionStorage.setItem('user-token', data.token);
+            sendData({type: 'join-game', token: data.token, username: localStorage.getItem('username')});
+       });
+       tokenListener.listen();
+    }
+}
 
 
 
@@ -87,12 +79,15 @@ addEventListener('DOMContentLoaded', () => {//script is at the top of the DOM, l
     gameListener.listen();
     
     var statsListener = new Listener('server-stats', function(data){
+        console.log('started listening for stats');
         let playerCount = document.querySelector('#players');
         let gameCount = document.querySelector('#games');
         let connectionCount = document.querySelector('#connections');
-        playerCount.innerText += data.players.toString();
-        gameCount.innnerText += data.games.toString();
-        connectionCount.innerText += data.webSocketConnections.toString();
+        playerCount.innerText =`Players in game: ${data.players}`;
+        gameCount.innerText = `Ongoing games: ${data.games}`;
+        connectionCount.innerText = `WebSocket connection: ${data.webSocketConnections}`;
+        
+        
     });
     statsListener.listen();
 
@@ -102,6 +97,6 @@ addEventListener('DOMContentLoaded', () => {//script is at the top of the DOM, l
     username.addEventListener('change', ()=>{localStorage.setItem('username',username.value)});//if the username changes, update changes
 
     sendData({ type: 'get-server-stats' });
-    setInterval(() => sendData({ type: 'get-server-stats' }), 500);//poll server stats in 0.5s interval
+    setInterval(() => sendData({ type: 'get-server-stats' }), 1000);//poll server stats in 0.5s interval
 
 });
